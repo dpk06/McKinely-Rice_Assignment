7fdf8a715c83f1dae9eafbe2632a4af05ebfad75
Pooja <anbhulepooja250@gmail.com>
2020-03-05 14:14:25 +0530

    Initial Commit

---
+++ McKinely&Rice_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  McKinely&Rice_Assignment
+//
+//  Created by Deep on 05/03/20.
+//  Copyright © 2020 Deepak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
